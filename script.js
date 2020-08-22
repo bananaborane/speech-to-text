ff6ad520296effacb402f1903c527ab1db408cba
LORENZO G <lorenzog@LORENZOs-MacBook-Pro.local>
2020-08-22 15:34:24 -0700

    speech to text first commit, included start and stop recog button

---
+++ script.js
@@ -0,0 +1,65 @@
+let speechRecognition = window.webkitSpeechRecognition
+
+let recognition = new speechRecognition()
+
+let textbox = $('#textbox')
+
+let instructions = $('#instructions')
+
+let content = ''
+
+let recording = false;
+
+recognition.continuous = true
+
+// recognition is started
+
+recognition.onstart = function (){
+    instructions.text("Voice Recognition is on")
+}
+
+recognition.onspeechend = function(){
+    instructions.text("No Activity")
+}
+
+recognition.onerror = function (){
+    instructions.text("Try Again")
+}
+
+recognition.onresult = function (event){
+    let current = event.resultIndex;
+
+    let transcript = event.results[current][0].transcript
+
+    content += transcript
+
+    textbox.val(content)
+}
+
+recognition
+
+$('#start-btn').click(function(event){
+    if (content.length){
+        content += ''
+    }
+
+    if (recording){
+        content += ' '
+        $(this).removeClass('btn-danger')
+        $(this).text("Start")
+        recognition.stop()
+        recording = false
+        return
+    }
+    
+    recording = true;
+    $(this).addClass('btn-danger')
+    $(this).text("Stop")
+    recognition.start()
+})
+
+
+
+textbox.on('input', function(){
+    content = $(this).val()
+})
